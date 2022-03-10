00eb3fd888a51497d5cdd402ecbf08de5c193a9b
THIAGO DO NASCIMENTO MENEGUZZI <thiago.meneguzzi@alunos.sc.senac.br>
2022-03-10 08:12:18 -0300

    .

---
+++ src/model/dao/EnderecoDAO.java
@@ -0,0 +1,32 @@
+package model.dao;
+
+import model.entity.Endereco;
+
+public class EnderecoDAO {
+
+    // TODO inserir
+    public Endereco criarEndereco(Endereco novoEndereco) {
+        return null;
+    }
+
+    // TODO atualizar
+    public boolean atualizarEndereco(Endereco enderecoAtualizado) {
+        return true;
+    }
+
+    // TODO consultar 1 (por id)
+    public Endereco buscarUnicoEndereco() {
+        return null;
+    }
+
+    // TODO consultar todos
+    public Endereco buscarTodosEndereco() {
+        return null;
+    }
+
+    // TODO remover
+    public boolean removerEndereco(int idEndereco) {
+        return true;
+    }
+
+}
